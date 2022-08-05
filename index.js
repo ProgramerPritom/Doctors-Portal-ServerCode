2c784ed10f76e09618552747f33d90fb01501b3d
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-08-05 23:25:42 +0600

    added verify admin with JWT token and create doctor details with database

--- index.js
+++ index.js
@@ -14,33 +14,50 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqfzs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res,next){
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({message: 'UnAuthorized access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if (err) {
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         const userCollection = client.db('doctors_portal').collection('user');
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
+        const verifyAdmin = async(req,res,next) =>{
 
-      function verifyJWT(req,res,next){
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-          return res.status(401).send({message: 'UnAuthorized access'});
-        }
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-          if (err) {
-            return res.status(403).send({message: 'Forbidden access'})
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({email: requester});
+          if (requesterAccount.role === 'admin') {
+            next();
           }
-          req.decoded = decoded;
-          next();
-        });
-      }
+          else{
+            res.status(403).send({message: 'forbidden'});
+          }
+
+        }
+
+        
+      
 
 
+        
         app.get('/service', async(req,res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({name: 1});
             const result = await cursor.toArray();
             res.send(result);
         })
@@ -57,12 +74,10 @@ async function run(){
             res.send({admin: isAdmin});
           })
 
-        app.put('/user/admin/:email',verifyJWT, async (req,res) => {
+        app.put('/user/admin/:email',verifyJWT, verifyAdmin, async (req,res) => {
           const email = req.params.email;
-          const requester = req.decoded.email;
-          const requesterAccount = await userCollection.findOne({email: requester});
-          if (requesterAccount.role === 'admin') {
-            const filter = {email: email};
+          
+          const filter = {email: email};
           
           const updateDoc = {
             $set: {role: 'admin'},
@@ -70,10 +85,6 @@ async function run(){
           const result = await userCollection.updateOne(filter, updateDoc);
          
           res.send(result);
-          }
-          else{
-            res.status(403).send({message: 'forbidden'});
-          }
           
         })
 
@@ -88,6 +99,18 @@ async function run(){
           const result = await userCollection.updateOne(filter, updateDoc, options);
           const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '4445h' } );
           res.send({result, token});
+        })
+        // Doctors collection
+
+        app.get('/doctor',verifyJWT,verifyAdmin, async(req,res)=>{
+          const result = await doctorCollection.find().toArray();
+          res.send(result);
+        })
+
+        app.post('/doctor', verifyJWT, verifyAdmin, async(req,res)=>{
+          const doctor = req.body;
+          const result = await doctorCollection.insertOne(doctor);
+          res.send(result);
         })
 
 
